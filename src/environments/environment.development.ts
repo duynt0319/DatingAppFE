9ea734d21aa8b1ae56bfc38bcb4f6d21f199aa08
Nguyen Thanh Duy <duynt0319@gmail.com>
2024-03-11 02:54:25 +0700

    done signal R

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7076/api/'
+    apiUrl: 'https://localhost:7076/api/',
+    hubUrl: 'https://localhost:7076/hubs/'
 };
